2769c016bbd193a563159e72db4867dc1818354f
kajahun123 <65674181+kajahun123@users.noreply.github.com>
2020-11-30 17:28:54 +0100

    Add files via upload

--- Database/CreateBooks.sql
+++ Database/CreateBooks.sql
@@ -1,6 +1,7 @@
 DROP TABLE konyvek;
 
 CREATE TABLE konyvek(
+	kolcsonzes_id int not null,
     olvasojegy_code char(8) not null,
 	kolcsonzo_nev varchar2(100) not null,
 	cim varchar(100) not null,
@@ -8,7 +9,7 @@ CREATE TABLE konyvek(
 	mufaj varchar2(100) not null,
 	kolcsonzes_date date not null,
 	
-	constraint pk_konyvek primary key(olvasojegy_code),
+	constraint pk_kolcsonzes_id primary key(kolcsonzes_id),
 	constraint fk_irok foreign key(iro_id) references irok(id)
 	
 );
