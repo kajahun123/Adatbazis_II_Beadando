5cc08b6b6929c32f27dcc815f393ce2f41dbd329
kajahun123 <65674181+kajahun123@users.noreply.github.com>
2020-11-30 17:27:53 +0100

    Add files via upload

--- Database/InsertKonyvek.sql
+++ Database/InsertKonyvek.sql
@@ -12,14 +12,23 @@ CREATE OR REPLACE PROCEDURE spInsert_konyvek(
 AUTHID DEFINER
 AS
 	v_check_olvasojegy int;
+	v_kolcsonzes_id int;
 BEGIN
 	p_out_rowcnt := 0;
 	
 	IF v_check_olvasojegy = 1 THEN
+	
+		SELECT MAX(kolcsonzes_id) INTO v_kolcsonzes_id FROM konyvek;
+		
+		IF v_kolcsonzes_id IS NULL THEN
+			v_kolcsonzes_id := 0;
+		END IF;
+		v_id := v_id + 1;
+		
 		INSERT INTO konyvek
-			(olvasojegy_code, kolcsonzo_nev, cim, iro_id, mufaj, kolcsonzes_date)
+			(olvasojegy_code, kolcsonzo_nev, cim, iro_id, mufaj, kolcsonzes_date, kolcsonzes_id)
 		VALUES 
-        (p_olvasojegy, p_kolcsozno_nev, p_cim, p_iro, p_mufaj, p_kolcsonzes_date);
+        (p_olvasojegy, p_kolcsozno_nev, p_cim, p_iro, p_mufaj, p_kolcsonzes_date, v_kolcsonzes_id);
 		
         p_out_rowcnt := SQL%rowcount;
 		COMMIT;
