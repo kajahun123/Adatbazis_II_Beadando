2d12008cdb172eb8d6ac523acf1d34d9beaa6589
kajahun123 <65674181+kajahun123@users.noreply.github.com>
2020-11-30 16:32:25 +0100

    Add files via upload

--- Database/InsertKonyvek.sql
+++ Database/InsertKonyvek.sql
@@ -1,11 +1,10 @@
 CREATE OR REPLACE PROCEDURE spInsert_konyvek(
-	p_olvasojegy 		   in konyvek.olvasojegy_code%type,
-	p_first_name 	        in konyvek.first_name%type,
-	p_last_name		        in konyvek.last_name%type,
+	p_olvasojegy 		   	in konyvek.olvasojegy_code%type,
+	p_kolcsozno_nev	        in konyvek.kolcsonzo_nev%type,
 	p_cim		            in konyvek.cim%type,
 	p_iro	                in iro.nev%type,
 	p_mufaj		            in konyvek.mufaj%type,
-	p_kiadas		        in konyvek.kiadas%type,
+	p_kolcsonzes_date       in konyvek.kolcsonzes_date%type,
 	
 	p_out_rowcnt out int
 )
@@ -18,9 +17,9 @@ BEGIN
 	
 	IF v_check_olvasojegy = 1 THEN
 		INSERT INTO konyvek
-			(olvasojegy_code, first_name, last_name, cim, iro_id, mufaj, kiadas)
+			(olvasojegy_code, kolcsonzo_nev, cim, iro_id, mufaj, kolcsonzes_date)
 		VALUES 
-        (p_olvasojegy, p_first_name, p_last_name, p_cim, p_iro, p_mufaj, p_kiadas);
+        (p_olvasojegy, p_kolcsozno_nev, p_cim, p_iro, p_mufaj, p_kolcsonzes_date);
 		
         p_out_rowcnt := SQL%rowcount;
 		COMMIT;
